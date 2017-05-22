16fb878eaa92ced39530f84cc52b4be91fff4cf3
Jonathan Allenberg <m@jfa.berlin>
2017-05-22 17:47:51 +0200

    model

--- src/model/User.js
+++ src/model/User.js
@@ -1,111 +1,16 @@
 // @flow
 
-
-// import mongoose from 'mongoose';
-// import bcrypt from 'bcrypt-as-promised';
-
-// const Schema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   password: {
-//     type: String,
-//     hidden: true,
-//   },
-//   email: {
-//     type: String,
-//     required: false,
-//     index: true,
-//   },
-//   active: {
-//     type: Boolean,
-//     default: true,
-//   },
-// }, {
-//   timestamps: {
-//     createdAt: 'createdAt',
-//     updatedAt: 'updatedAt',
-//   },
-//   collection: 'user',
-// });
-
-// Schema
-//   .pre('save', function (next) {
-//     // Hash the password
-//     if (this.isModified('password')) {
-//       this.encryptPassword(this.password)
-//         .then((hash) => {
-//           this.password = hash;
-//           next();
-//         })
-//         .catch(err => next(err));
-//     } else {
-//       return next();
-//     }
-//   });
-
-// Schema.methods = {
-//   async authenticate(plainTextPassword) {
-//     try {
-//       return await bcrypt.compare(plainTextPassword, this.password);
-//     } catch (err) {
-//       return false;
-//     }
-//   },
-//   encryptPassword(password) {
-//     return bcrypt.hash(password, 8);
-//   },
-// };
-
-
-// export default mongoose.model('User', Schema);
-
-//
-
-import arangoose from 'arangoose';
+import Arangoose from '@jfa/arangoose';
 import bcrypt from 'bcrypt-as-promised';
 
-class Password {
-  constructor(password){
-    this.hash = bcrypt.hash(password, 8)
+const User = class User extends Arangoose.Model {
+  static schema = {
+    name: String,
+    password: String,
+    email: String,
   }
-  async compare(plainTextPassword){
-    return await bcrypt.compare(plainTextPassword, this.hash);
-  }
-}
-
-const Schema = new arangoose.Schema({
-   name: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: Password,
-    hidden: true,
-  },
-  email: {
-    type: String,
-    required: false,
-    index: true,
-  },
-  active: {
-    type: Boolean,
-    default: true,
-  }
-})
-
-const User = class extends arangoose.Model{
-  
-  static schema = Schema;
 
   async authenticate(plainTextPassword) {
-    try {
-      return await this.password.compare(plainTextPassword);
-    } catch (err) {
-      return false
-    }
-
     try {
       return await bcrypt.compare(plainTextPassword, this.password);
     } catch (err) {
@@ -113,9 +18,9 @@ const User = class extends arangoose.Model{
     }
   }
 
-  encryptPassword(password) {
+  static encryptPassword(password) {
     return bcrypt.hash(password, 8);
   }
-}
+};
 
-export default arangoose.model( User ,new Model)
+export default Arangoose.model(User);
